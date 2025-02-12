e3b91bed7b1e0b4e77e593b07a4f56a0ffa8afb6
guillermoll <guillermo.jobs@hotmail.com>
2025-02-12 12:02:49 +0000

    cambiado la forma en la que hacen los tests, ahora formato AAA

--- typescript-api/tests/users/user.test.ts
+++ typescript-api/tests/users/user.test.ts
@@ -2,29 +2,27 @@ import supertest from 'supertest'
 import initApiRest, { app } from '../../index'
 import { Server } from 'http'
 import TestAgent from 'supertest/lib/agent'
-import { MysqlConfig } from '../../src/users/models/mysql/mysql.config'
 
 describe('User Model tests', () => {
   let server: Server
   let api: TestAgent
-  
-  beforeAll(()=>{
+
+  // Arrange
+  beforeAll(() => {
     api = supertest(app)
     server = initApiRest(8081)
   })
 
   test('Get users whit sensitive data', async () => {
-    // Arrange
-
     // Act
-    await api.get('/users')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/users')
 
     // Assert
+    expect(response.statusCode).toBe(200)
+    expect(response.header['content-type']).toContain('application/json')
   })
 
-  afterAll(()=>{
+  afterAll(() => {
     server.close()
   })
 })
